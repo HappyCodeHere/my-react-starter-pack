7f95873a8382c92300b47d6e8c838343074b80ef
HappyLoL <happylolonly@gmai.com>
2016-12-16 00:33:06 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.scss';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <p>Hello, it is my cool React starter pack!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
